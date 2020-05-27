4f20a98477021cf1ea93d160fb8086e7db203664
Ellmo Rivetti <ellmorivetti@gmail.com>
2020-05-27 16:03:03 +0200

    add camera movement v1

--- Assets/Scripts/GameLoop.cs
+++ Assets/Scripts/GameLoop.cs
@@ -11,6 +11,10 @@ public class GameLoop : MonoBehaviour
 
     private List<GameObject> blockList;
     private bool currentBlock = false;
+
+    private float up_distance = 20f;
+    private float down_distance = 30f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +23,31 @@ public class GameLoop : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        float max_height = 0;
+        foreach (GameObject g in blockList)
+        {
+            if(g.tag == "set")
+            {
+                if (max_height < g.transform.position.y)
+                {
+                    max_height = g.transform.position.y;
+                }
+            }
+        }
 
+        //Debug.Log("Max height: " + max_height);
+        //Debug.Log("spawner height: " + spawner.transform.position.y);
+        
+        if (spawner.transform.position.y - 15 > max_height)
+        {
+            DownSpawnnerAndCam();
+        }
+        else if (spawner.transform.position.y - 13 < max_height)
+        {
+            UpSpawnnerAndCam();
+        }
     }
 
     void Spawn()
@@ -51,8 +77,48 @@ public class GameLoop : MonoBehaviour
         GameObject go = sender as GameObject;
         block.TouchRemover -= OnTouchRemover;
         currentBlock = false;
-        Debug.Log("avant " + blockList.Count);
+        //Debug.Log("avant " + blockList.Count);
         blockList.Remove(blockList.Find(x => x.GetComponent<TetrisBlock>().GetID()== block.GetID()));
-        Debug.Log("apres " + blockList.Count);
+        //Debug.Log("apres " + blockList.Count);
+    }
+
+    private void UpSpawnnerAndCam()
+    {
+        UpSpawnner();
+        UpCamera();
+    }
+
+    private void UpSpawnner()
+    {
+        //up spawner
+        MoveObject(spawner.transform, new Vector3(this.spawner.transform.position.x, this.spawner.transform.position.y + up_distance, this.spawner.transform.position.z), 0.3f);
+    }
+
+    private void UpCamera()
+    {
+        MoveObject(this.camera.transform, new Vector3(this.camera.transform.position.x, this.camera.transform.position.y + up_distance, this.camera.transform.position.z), 0.3f);
+    }
+
+    private void DownSpawnnerAndCam()
+    {
+        DownSpawnner();
+        DownCamera();
+    }
+
+    private void DownSpawnner()
+    {
+        //Down spawner
+        MoveObject(this.spawner.transform, new Vector3(this.spawner.transform.position.x, this.spawner.transform.position.y - down_distance, this.spawner.transform.position.z), 0.4f);
+    }
+
+    private void DownCamera()
+    {
+        //Down camera
+        MoveObject(this.camera.transform, new Vector3(this.camera.transform.position.x, this.camera.transform.position.y - down_distance, this.camera.transform.position.z), 0.4f);
+    }
+
+    private void MoveObject(Transform transform, Vector3 vec, float speed)
+    {
+        transform.position = Vector3.Lerp(transform.position, vec, speed * Time.deltaTime);
     }
 }
